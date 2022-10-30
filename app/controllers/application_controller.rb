0067fce498002433bb57f2cdc68777e8b040d135
Ubuntu <ubuntu@ip-172-31-46-180.ec2.internal>
2022-10-30 19:43:15 +0000

    second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
                 cart = Cart.create
                 session[:cart_id] = cart.id
             end
+            cart
         end
         helper_method :current_cart
 end
